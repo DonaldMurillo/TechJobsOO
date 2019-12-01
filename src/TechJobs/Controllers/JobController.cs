8e2f7c6d0ee6e2ad233c16890e9b3dc6f366d578
Donald Murillo <megalion07@hotmail.com>
2019-12-01 11:07:06 -0500

    Before DRY (task 7)

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -19,8 +20,13 @@ namespace TechJobs.Controllers
         public IActionResult Index(int id)
         {
             // TODO #1 - get the Job with the given ID and pass it into the view
+            SearchJobsViewModel searchJobsViewModel = new SearchJobsViewModel();
+            //Job newJob = new Job();
+            Job newJob = jobData.Find(id);
+            searchJobsViewModel.Jobs = new System.Collections.Generic.List<Job>();
+            searchJobsViewModel.Jobs.Add(newJob);
 
-            return View();
+            return View(searchJobsViewModel);
         }
 
         public IActionResult New()
@@ -35,6 +41,28 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    Location = jobData.Locations.Find(newJobViewModel.JobLocation),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.JobCoreCompetency),
+                    PositionType = jobData.PositionTypes.Find(newJobViewModel.JobPoitionType)
+                    // set properties within braces
+                };
+
+                jobData.Jobs.Add(newJob);
+
+                //return RedirectToAction("Job", "Index" , new { id = newJob.ID });
+
+                //return Redirect("Index?=" + newJob.ID.ToString());
+
+                //return RedirectToAction( "Job" ,new { id = newJob.ID });
+
+                return Redirect(Url.Action("Index") + "?id=" + newJob.ID);
+            }
 
             return View(newJobViewModel);
         }
